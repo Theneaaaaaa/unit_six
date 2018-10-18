1f8380e9b9301bffd7a81c55938d2ba2b2389fd5
Thenea <thenea@iMac4.local>
2018-10-18 14:29:35 -0400

    Second version

--- d1_unit_six_warmups.py
+++ d1_unit_six_warmups.py
@@ -1,10 +1,10 @@
-my_list = ("Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco")
+students = ("Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco")
 
 
-print(my_list[0])
+print(students[0])
 
 
-print(my_list[2])
+print(students[2])
 
 
-print(my_list[-1])
+print(students[-1])
