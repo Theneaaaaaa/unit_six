185703a97358d402e9784c89216385f4f90f2225
Thenea <thenea@iMac4.local>
2018-10-22 08:29:24 -0400

    working with list slices

---
+++ d2_unit_six_warmups.py
@@ -0,0 +1,22 @@
+students = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+
+print(students[0])
+
+
+print(students[1])
+
+
+print(students[2])
+
+
+print(students[3])
+
+
+print(students[4])
+
+
+print(students[3:])
+
+
+print(students[:6])
+
