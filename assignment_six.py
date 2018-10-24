74561ec64fbefac0a90cd3f485e84940fa449828
Thenea <thenea@iMac4.local>
2018-10-24 11:10:55 -0400

    This is so hard lol but it is really fun. I am chaging stuff when the letter is in the word, but there are too many bugs and it is not doing what I want it to do

--- assignment_six.py
+++ assignment_six.py
@@ -5,32 +5,42 @@
 # letter in a limited number of times. It has relationship with our current studying unit.
 
 
-import random()
+import random
 
-while True:
-    play = input("Would you like to start the game?")
-    if play in ["y", "Y", "Yes", "yes", "Sure", "Why not", "Let's go", "Start", "start", "ya", "Yeah", "yeah", "yay"]:
-        print("Nice! Let's start the Squeal of Fortune now!")
-        break
-    else:
-        print("Oh, I am feeling sad now, see you next time then!")
-        break
+# while True:
+#     play = input("Would you like to start the game?")
+#     if play in ["y", "Y", "Yes", "yes", "Sure", "Why not", "Let's go", "Start", "start", "ya", "Yeah", "yeah", "yay"]:
+#         print("Nice! Let's start the Squeal of Fortune now!")
+#         break
+#     else:
+#         print("Oh, I am feeling sad now, see you next time then!")
+#         break
 
 
 words = ["aberration", "abject", "accede", "acrimony", "adulation", "affinity", "aloof", "amicable", "antecedent",
-             "antiquated", "arbitrary", "ascribe", "assuage", "austere", "behemoth", "benevolent", "buttress",
-             "camaraderie", "caustic", "chide", "circumlocution", "cognizant", "commensurate", "compunction", "concoct",
-             "connive", "contravene", "convoluted", "culpable", "debacle", "defunct", "denigrate", "derelict", "devious"
-             , "discretion", "disparage", "divulge", "ebullient", "egregious", "emaciated", "emollient", "enmity"
-             , "evanescent", "evince", "expiate", "fabricate", "fetter", "foil", "fortuitous", "goad", "harrowing",
-             "harrowing", "hiatus", "imperative", "impervious", "inane", "incontrovertible", "indomitable"]
+         "antiquated", "arbitrary", "ascribe", "assuage", "austere", "behemoth", "benevolent", "buttress",
+         "camaraderie", "caustic", "chide", "circumlocution", "cognizant", "commensurate", "compunction", "concoct",
+         "connive", "contravene", "convoluted", "culpable", "debacle", "defunct", "denigrate", "derelict", "devious"
+         , "discretion", "disparage", "divulge", "ebullient", "egregious", "emaciated", "emollient", "enmity"
+         , "evanescent", "evince", "expiate", "fabricate", "fetter", "foil", "fortuitous", "goad", "harrowing",
+         "harrowing", "hiatus", "imperative", "impervious", "inane", "incontrovertible", "indomitable"]
 
 
-def choose_word():
-    random.choice(words)
-    return words
+word = random.choice(words)
+blank_list = []
 
-def fill_blanks():
-    blank_list = []
-    for x in range(len(choose_word())):
-        
+for x in range(len(word)):
+    blank_list.append("_")
+
+print(blank_list)
+
+for x in range(7):
+    letter = input("Please guess a letter:")
+    if letter in word:
+        print(letter, "is in the word!")
+        blank_list.append(letter)
+        print(blank_list)
+    else:
+        print("Sorry, but the letter is not in the word.")
+
+
