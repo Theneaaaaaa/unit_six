9602dcc3269d7eaf0d6607e2f299a5aa213850f7
Thenea <thenea@iMac4.local>
2018-10-25 12:06:25 -0400

    Close to finish. It is getting harder and harder.

--- assignment_six.py
+++ assignment_six.py
@@ -7,15 +7,6 @@
 
 import random
 
-# while True:
-#     play = input("Would you like to start the game?")
-#     if play in ["y", "Y", "Yes", "yes", "Sure", "Why not", "Let's go", "Start", "start", "ya", "Yeah", "yeah", "yay"]:
-#         print("Nice! Let's start the Squeal of Fortune now!")
-#         break
-#     else:
-#         print("Oh, I am feeling sad now, see you next time then!")
-#         break
-
 
 words = ["aberration", "abject", "accede", "acrimony", "adulation", "affinity", "aloof", "amicable", "antecedent",
          "antiquated", "arbitrary", "ascribe", "assuage", "austere", "behemoth", "benevolent", "buttress",
@@ -28,19 +19,44 @@ words = ["aberration", "abject", "accede", "acrimony", "adulation", "affinity", 
 
 word = random.choice(words)
 blank_list = []
+# in order to make a blank list with "_" for the words
 
-for x in range(len(word)):
-    blank_list.append("_")
 
-print(blank_list)
+def starting():
+    for x in range(len(word)):
+        blank_list.append("_")
+    print(blank_list)
 
-for x in range(7):
-    letter = input("Please guess a letter:")
-    if letter in word:
-        print(letter, "is in the word!")
-        blank_list.append(letter)
-        print(blank_list)
-    else:
-        print("Sorry, but the letter is not in the word.")
 
+def guess():
+    number_guesses = 7
+    for x in range(number_guesses):
+        for x in range(len(word)):
+            letter = input("Please guess a letter:")
+            if letter == word[x]:
+                print(letter, "is in the word!")
+                blank_list[x] = letter
+                print(blank_list)
+            else:
+                print("Sorry, but the letter is not in the word.")
+                break
+#
+
+
+def main():
+    while True:
+        play = input("Would you like to start the game?")
+        if play in ["y", "Y", "Yes", "yes", "Sure", "Why not", "Let's go", "Start", "start", "ya", "Yeah", "yeah",
+                    "yay"]:
+            print("Nice! Let's start the Squeal of Fortune now!")
+            number_guesses = 7
+            while number_guesses > 0 or "_" in word:
+                starting()
+                guess()
+        else:
+            print("Oh, I am feeling sad now, see you next time then!")
+            break
+
+
+main()
 
