e8e6a0025bc23875971b8043c27003b29dc696a9
Thenea <thenea@iMac4.local>
2018-10-26 15:05:20 -0400

    Tried too hard on that, and still did not solve the problem. I am a little sad now :/

--- assignment_six.py
+++ assignment_six.py
@@ -8,53 +8,38 @@
 import random
 
 
-words = ["aberration", "abject", "accede", "acrimony", "adulation", "affinity", "aloof", "amicable", "antecedent",
-         "antiquated", "arbitrary", "ascribe", "assuage", "austere", "behemoth", "benevolent", "buttress",
-         "camaraderie", "caustic", "chide", "circumlocution", "cognizant", "commensurate", "compunction", "concoct",
-         "connive", "contravene", "convoluted", "culpable", "debacle", "defunct", "denigrate", "derelict", "devious"
-         , "discretion", "disparage", "divulge", "ebullient", "egregious", "emaciated", "emollient", "enmity"
-         , "evanescent", "evince", "expiate", "fabricate", "fetter", "foil", "fortuitous", "goad", "harrowing",
-         "harrowing", "hiatus", "imperative", "impervious", "inane", "incontrovertible", "indomitable"]
-
-
-word = random.choice(words)
-blank_list = []
-# in order to make a blank list with "_" for the words
-
-
-def starting():
-    for x in range(len(word)):
-        blank_list.append("_")
-    print(blank_list)
-
-
-def guess():
-    number_guesses = 7
-    for x in range(number_guesses):
-        for x in range(len(word)):
-            letter = input("Please guess a letter:")
-            if letter == word[x]:
-                print(letter, "is in the word!")
-                blank_list[x] = letter
-                print(blank_list)
-            else:
-                print("Sorry, but the letter is not in the word.")
-                break
-#
-
-
 def main():
+    words = ["aberration", "abject"]
+    word = random.choice(words)
+    blank_list = []
+    number_guesses = 7
+    number_guessed = 0
+    # in order to make a blank list with "_" for the words
     while True:
         play = input("Would you like to start the game?")
         if play in ["y", "Y", "Yes", "yes", "Sure", "Why not", "Let's go", "Start", "start", "ya", "Yeah", "yeah",
                     "yay"]:
             print("Nice! Let's start the Squeal of Fortune now!")
-            number_guesses = 7
-            while number_guesses > 0 or "_" in word:
-                starting()
-                guess()
+            for x in range(len(word)):
+                    blank_list.append("_")
+            while number_guesses > 0 or "_" not in blank_list:
+                print(blank_list)
+                letter = input("Please guess a letter:")
+                if letter in word:
+                    for x in range(len(word)):
+                        if letter == word[x]:
+                            blank_list[x] = letter
+    # This is one of the most important function in this code. It helps with correcting the guesses
+                else:
+                    print("Sorry, but the letter is not in the word.")
+                    number_guessed = number_guessed + 1
+                    if number_guessed >= 7:
+                        print("You had used all your chances, sorry, you lost.")
+                while "_" not in blank_list:
+                    print("Congratulation! You got the word!")
+                    break
         else:
-            print("Oh, I am feeling sad now, see you next time then!")
+            print("Oh, that's sad, see you next time then!")
             break
 
 
